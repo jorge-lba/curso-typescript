ce9e8ce7192c527ef8c64b9f01ed89d32e3dc3f4
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-16 19:31:25 -0300

    Add - Finalizado modulo de configuraçoes do TypeScript

--- configs/index.ts
+++ configs/index.ts
@@ -3,4 +3,19 @@ let subscribers:number = 610268
 
 channel = 'subscribers'
 
+function salute(isMorning:string, hours:number): string{
+
+    let salutation:string
+
+    if(isMorning){
+        salutation = 'Bom dia'
+        console.log(`Horário: ${hours}`)
+    }else{
+        salutation = 'Olá'
+    }
+
+    return salutation
+}
+
+console.log( salute('Bom dia', 11) )
 console.log(channel)
