8b562be5df3b250ee9d53710ab2ec31cfbb4ec00
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-16 01:23:26 -0300

    Add - Tipos mesclados

---
+++ types/typesUnion.js
@@ -0,0 +1,6 @@
+"use strict";
+var points = 10;
+console.log("Eu tenho " + points + " pontos.");
+points = '52';
+console.log("Eu tenho " + points + " pontos.");
+module.exports = {};
---
+++ types/typesUnion.ts
@@ -0,0 +1,7 @@
+export={}
+
+let points: number | string = 10
+console.log( `Eu tenho ${points} pontos.` )
+
+points = '52'
+console.log(`Eu tenho ${points} pontos.`)
