f003519fe92f04ec430f3fdeac323708731878c9
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-16 01:34:54 -0300

    Add - Tipo Never

---
+++ types/typesNever.js
@@ -0,0 +1,19 @@
+"use strict";
+//Never é um tipo onde uma função não pode ser comcluida, sendo um loop infinito ou um erro
+function error(message) {
+    throw new Error(message);
+}
+var product = {
+    name: 'Caderno',
+    price: -1,
+    validateProduct: function () {
+        if (!this.name || this.name.trim().length == 0) {
+            error("O Produto precisa ter um nome");
+        }
+        if (this.price <= 0) {
+            error("Preço invalido");
+        }
+    }
+};
+console.log(product.validateProduct());
+module.exports = {};
---
+++ types/typesNever.ts
@@ -0,0 +1,22 @@
+export={}
+
+//Never é um tipo onde uma função não pode ser comcluida, sendo um loop infinito ou um erro
+
+function error(message:string):never{
+    throw new Error(message)
+}
+
+const product = {
+    name: 'Caderno',
+    price: -1,
+    validateProduct(){
+        if(!this.name || this.name.trim().length== 0){
+            error("O Produto precisa ter um nome")
+        }
+        if(this.price <= 0 ){
+            error("Preço invalido")
+        }
+    }
+}
+
+console.log(product.validateProduct())
