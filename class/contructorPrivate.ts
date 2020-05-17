ad1636fbf07195990004418798cb2ce57e762947
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-17 17:48:32 -0300

    Add - Aula sobre atributos somente leitura

--- class/contructorPrivate.ts
+++ class/contructorPrivate.ts
@@ -14,3 +14,19 @@ class Unic {
 }
 
 console.log(Unic.instance().now())
+
+// Somente leitura
+
+class Airplane{
+    public readonly model:string
+
+    constructor(
+        model:string,
+        public readonly prefix:string
+    ){
+        this.model = model
+    }
+}
+
+const turboH:Airplane = new Airplane('Tu-114', 'PT-ABC')
+console.log(turboH.model, turboH.prefix) 
