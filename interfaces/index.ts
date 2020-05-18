c82ab8fa9be0350ebcaa8df29cfbac5392f001a2
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-17 22:33:09 -0300

    Add - Finalizado o modulo de interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,6 +1,8 @@
 interface Person {
     name:string,
-    age:number
+    age?:number,
+    [prop:string]:any
+    greet(lastName:string):void
 }
 
 function greetWithHello(person:Person){
@@ -11,12 +13,62 @@ function changeName(person:Person){
     person.name = 'Luiz'
 }
 
-const person = {
+const person:Person  = {
     name: 'Jorge',
-    age: 27
+    age: 27,
+    greet(lastName:string):void{
+        console.log('Olá ' + lastName)
+    }
 }
 
 changeName(person)
 greetWithHello(person)
+person.greet('Jorge Alegretti')
+// greetWithHello({name: 'Carlos', age:35, test:true})
+
+class Client implements Person {
+    name:string = ''
+    lastPurchase:Date = new Date
+
+    greet(lastName:string):void{
+        console.log( `Olá, meu nome é ${this.name} ${lastName}` )
+    }
+}
+
+const client:Client = new Client
+client.name = 'Jorge'
+client.greet('Alegretti')
+console.log(client.lastPurchase)
+
+interface CalculationFunction {
+    (a:number, b:number):number
+}
+
+let power:CalculationFunction
+
+power = (base:number, exp:number ):number => base ** exp
+
+console.log( power(8,2) )
+console.log(
+    Array(2).fill(8).reduce((t, a) => t * a)
+)
+
+// interface Object{
+//     log():void
+// }
+
+// Object.prototype.log = function(){
+//     console.log(this.toString())
+// }
+
+// const x = 2
+// const y = 3
+// const z = 4
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// x.log()
 
 export = {}
